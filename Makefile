22add5e8629f5c72511b5b3f63cb96b515699d97
senexi <flo@floland.de>
2021-05-21 12:43:29 +0200

    fixes for arm version

--- Makefile
+++ Makefile
@@ -7,9 +7,9 @@ help: ## This help.
 	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
 
 build_base: ## Build the container
-	cd base && bash build.sh
+	cd base && bash build.sh $(ARCH)
 build_devops: ## Build the container
-	cd devops && bash build.sh
+	cd devops && bash build.sh $(ARCH)
 
 build: build_base build_devops
 	echo "build all"
--- base/build.sh
+++ base/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ARCH=${1:-"amd64"}
 ARCH2=${2:-"x86_64"}
-
+ARCH3=$ARCH
 TARGET="go-ide"
 BUILDER="docker"
 
@@ -14,7 +14,8 @@ fi
 if [ $ARCH = "arm64v8" ]
 then
   ARCH2="aarch64"
+  ARCH3="arm64"
 fi
 
 echo "using arch $ARCH $ARCH2"
-$BUILDER build --build-arg ARCH=$ARCH --build-arg ARCH2=$ARCH2 -t $TARGET:$ARCH-latest -f ./Containerfile .
+$BUILDER build --build-arg ARCH=$ARCH --build-arg ARCH2=$ARCH2 --build-arg ARCH3=$ARCH3 -t $TARGET:$ARCH-latest -f ./Dockerfile .
